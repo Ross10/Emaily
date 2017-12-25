f815fca4f083caa50e3451efc51407398a6073d1
Itai <itairusso0@gmail.com>
2017-12-25 17:11:22 +0200

    changed greeting

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 //A route handler to watch for incoming requestes with this method.
 app.get('/', (req,res) => {
-    res.send({hi: 'there'});
+    res.send({bye: 'buddy'});
 });
 
 // If heroku gave us a port to use - than use it - otherwise use 5000 on localhost.
