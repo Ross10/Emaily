293ccf1eef9b8bf1461103cf90b422bd4741be31
Itai <itairusso0@gmail.com>
2017-12-25 16:07:28 +0200

    Initail commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+//A route handler to watch for incoming requestes with this method.
+app.get('/', (req,res) => {
+    res.send({hi: 'there'});
+});
+
+// If heroku gave us a port to use - than use it - otherwise use 5000 on localhost.
+const PORT  = process.env.PORT || 5000;
+
+app.listen(PORT);
