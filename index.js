953ed75e57f732879b864fc8faa38b1a65dd7514
Itai <itairusso0@gmail.com>
2017-12-31 16:42:12 +0200

    fixing heroku issues

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const keys = require('./config/keys');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
-mongoose.connect(keys.mongoURI);
 
+
+//Connect to our DB.
+mongoose.connect(keys.mongoURI);
 const app = express();
 
+//The maxAge is on MilSec.
 app.use(
-  cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey]
-  })
+    cookieSession({
+       maxAge: 30 * 24 *60 *60 * 1000,//How long the cockie can be validate - Here the cockie will be last for 30 days. 
+       keys: [keys.cookieKey]//the coockieKey needs to be encrypt.
+    })
 );
+
 app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+// If heroku gave us a port to use - than use it - otherwise use 5000 on localhost.
+// const PORT  = process.env.PORT || 5002;
+// console.log("port is : " ,PORT);
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,21 +1,24 @@
+//Nevigation to different urls.
+
 const passport = require('passport');
 
-module.exports = app => {
-  app.get(
-    '/auth/google',
-    passport.authenticate('google', {
-      scope: ['profile', 'email']
-    })
-  );
 
-  app.get('/auth/google/callback', passport.authenticate('google'));
+module.exports = (app) => {
+    // A route handler to watch for incoming requestes with this method.
+    app.get('/', (req,res) => {
+        res.send({bye: 'buddy'});
+    });
 
-  app.get('/api/logout', (req, res) => {
-    req.logout();
-    res.send(req.user);
-  });
+    app.get('/auth/google',passport.authenticate('google', { scope: ['profile', 'email']}))
+    app.get('/auth/google/callback',passport.authenticate('google'));
+    app.get('/api/logout', (req,res) => {
+        req.logout(); // Its delete the cookie ID. Its a passport function.
+        res.send(req.user);
+    });
+    app.get('/api/current_user', (req,res)=> {
+        res.send(req.session);
+    });
 
-  app.get('/api/current_user', (req, res) => {
-    res.send(req.user);
-  });
 };
+
+
