c1099a98e9279d03e896873ec61ca5a8247fd0b2
Itai <itairusso0@gmail.com>
2017-12-31 16:44:18 +0200

    fixing heroku issues

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ require('./routes/authRoutes')(app);
 // const PORT  = process.env.PORT || 5002;
 // console.log("port is : " ,PORT);
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3000,  function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
